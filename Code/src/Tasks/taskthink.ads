8b93179ea37479eefe2b646d66935f6ef3eb9a74
John Frederick Lærum <71882170+Joffeey@users.noreply.github.com>
2022-11-17 16:48:51 +0100

    Delete Code directory

--- Code/src/Tasks/taskthink.ads
+++
@@ -1,9 +0,0 @@
-with MyMotorDriver; use MyMotorDriver;
-with MyBrain; use MyBrain;
-with DrivingStates; use DrivingStates;
-
-package TaskThink is
-
-   task Think with Priority=> 1;
-  
-end TaskThink;
